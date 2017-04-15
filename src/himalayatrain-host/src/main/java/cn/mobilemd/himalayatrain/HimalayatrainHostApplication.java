648287c5b6e185bd3dc1ba314551b38b411993e4
jerry <jerrymiao@renqidai.cc>
2017-04-15 09:06:09 +0800

    add

---
+++ src/himalayatrain-host/src/main/java/cn/mobilemd/himalayatrain/HimalayatrainHostApplication.java
@@ -0,0 +1,12 @@
+package cn.mobilemd.himalayatrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimalayatrainHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimalayatrainHostApplication.class, args);
+	}
+}
